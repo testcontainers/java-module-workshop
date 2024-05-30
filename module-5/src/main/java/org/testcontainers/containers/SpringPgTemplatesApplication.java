6c3b7e3c89f880b39ef28af3ea16004021fc55a7
shelajev <shelajev@gmail.com>
2024-05-30 15:12:40 +0200

    module 5 - postgres with templates - schema and app

---
+++ module-5/src/main/java/org/testcontainers/containers/SpringPgTemplatesApplication.java
@@ -0,0 +1,13 @@
+package org.testcontainers.containers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPgTemplatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPgTemplatesApplication.class, args);
+    }
+
+}
